1c4e6b92dbe75e9bee5cf190bdda5c3fa977c51a
Attemu <jhnsbs@gmail.com>
2018-11-16 14:10:28 +0100

    estoy en ello, no sulfurarse

--- Proyecto/src/servicios/ServiciosCategoria.java
+++ Proyecto/src/servicios/ServiciosCategoria.java
@@ -1,5 +1,34 @@
 package servicios;
 
-public class ServiciosCategoria {
+import java.util.ArrayList;
+
+import dao.DAOException;
+import model.Categoria;
+
+public class ServiciosCategoria implements OperacionesImpl<Categoria> {
+
+	@Override
+	public void annadir(Categoria objeto) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificar(Categoria objeto) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void borrar(Categoria objeto) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Categoria> mostrarTodo() throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
