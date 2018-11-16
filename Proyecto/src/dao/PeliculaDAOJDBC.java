cd08c6d822ae1c8d6660ce8c4114979a6841514a
Attemu <jhnsbs@gmail.com>
2018-11-16 11:59:04 +0100

    modificar pelicula

--- Proyecto/src/dao/PeliculaDAOJDBC.java
+++ Proyecto/src/dao/PeliculaDAOJDBC.java
@@ -4,10 +4,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import model.Pelicula;
 import model.Usuario;
 
 public class PeliculaDAOJDBC {
-	
+
 	private Connection con = null;
 
 	public PeliculaDAOJDBC() {
@@ -17,12 +18,27 @@ public class PeliculaDAOJDBC {
 	public void annadirPelicula(Pelicula film) throws DAOException {
 		try (Statement stmt = con.createStatement()) {
 			String query = "INSERT INTO PELICULA VALUES (" + film.getIdPelicula() + "," + "'" + film.getNombre() + "',"
-				 + film.getAnno() + ","  + film.getCategoria() + "," + film.getVistas() + "," + film.getValoracion() + ")";
+					+ film.getAnno() + "," + film.getCategoria() + "," + film.getVistas() + "," + film.getValoracion()
+					+ ")";
 			if (stmt.executeUpdate(query) != 1) {
-				throw new DAOException("Error añadiendo usuario");
+				throw new DAOException("Error al añadir pelicula");
 			}
 		} catch (SQLException se) {
-			throw new DAOException("Error añadiendo usuario en DAO", se);
+			throw new DAOException("Error añadiendo pelicula en DAO", se);
+		}
+	}
+
+	public void modificarPelicula(Pelicula film) throws DAOException {
+		try (Statement stmt = con.createStatement()) {
+			String query = "UPDATE PELICULA SET ID_PELICULA='" + film.getId() + "',   NOMBRE_PEL='"+film.getNombre()+"', ANNO_ESTRENO= "+film.getAnno()+
+								"', CATEGORIA_ID = '"+film.getCategoria()+"', VISTAS = '"+film.getVista()+"', VALORACION= '"+film.getValoracion()+
+								"WHERE NOMBRE = '" + film.getNombre();
+			if (stmt.executeUpdate(query) != 1) {
+				throw new DAOException("Error modificando la pelicula");
+			}
+
+		} catch (SQLException se) {
+			throw new DAOException("Error modificando pelicula en DAO", se);
 		}
 	}
 
